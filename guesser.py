4e98fb8fe34a57f4eb0a2351aea03a2072a849c9
Derya Durmush <81376000+eyadrmsh@users.noreply.github.com>
2025-03-15 22:21:26 +0100

    Update guesser.py
    edited functions descriptions

--- guesser.py
+++ guesser.py
@@ -6,16 +6,6 @@ import math
 import pandas as pd
 
 class Guesser:
-    '''
-        INSTRUCTIONS: This function should return your next guess. 
-        Currently it picks a random word from wordlist and returns that.
-        You will need to parse the output from Wordle:
-        - If your guess contains that character in a different position, Wordle will return a '-' in that position.
-        - If your guess does not contain thta character at all, Wordle will return a '+' in that position.
-        - If you guesses the character placement correctly, Wordle will return the character. 
-
-        You CANNOT just get the word from the Wordle class, obviously :)
-    '''
     def __init__(self, manual):
         self.word_list = yaml.load(open('wordlist.yaml'), Loader=yaml.FullLoader)
         self._manual = manual
